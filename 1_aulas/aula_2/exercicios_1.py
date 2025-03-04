bfd867802a1130f652d9737c4a99fab1c2e6f984
Gabriel Terra <gabrterra@gmail.com>
2025-03-04 11:09:20 -0300

    aula 2

--- 1_aulas/aula_2/exercicios_1.py
+++ 1_aulas/aula_2/exercicios_1.py
@@ -26,18 +26,55 @@ print(num_1 ** 2)
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+num_1 = float(input("Digite um numero: "))
+num_2 = float(input("Digite outro numero: "))
+print(num_1 + num_2)
+
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+num_1 = float(input("Digite um numero: "))
+num_2 = float(input("Digite outro numero: "))
+print((num_1 + num_2) / 2)
+
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+num_1 = float(input("Digite um numero: "))
+num_2 = float(input("Digite outro numero: "))
+print(num_1 ** num_2)
+
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+num_1 = float(input("Digite um numero: "))
+print((num_1 * 1.8) + 32)
+
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
+num_1 = float(input("Digite um numero: "))
+print(math.pi * (num_1 ** 2))
 
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+str_1 = input("Digite um texto: ")
+print(str_1.upper())
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+str_1 = input("Digite um texto: ")
+print(str_1.lower())
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+str_1 = input("Digite um texto: ")
+print(str_1.strip())
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data_input = input("Digite uma data no formato dd/mm/aaaa: ")
+lista_data = data_input.split("/")
+dia = lista_data[0]
+mes = lista_data[1]
+ano = lista_data[2]
+print(f"dia: {dia}, mês: {mes}, ano: {ano}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+str_1 = input("Digite um texto: ")
+str_2 = input("Digite outro texto: ")
+print(str_1 + str_2)
 
 # #### Booleanos (`bool`)
 
