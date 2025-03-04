a124de12839d28f8a6fdf66e9740986778689a32
Gabriel Terra <gabrterra@gmail.com>
2025-03-04 13:06:12 -0300

    aula 2

---
+++ 1_aulas/aula_2/1_try_except.py
@@ -0,0 +1,10 @@
+
+try:
+    resultado = len(3)
+    print(resultado)
+except TypeError as e:
+    print(e)
+else:
+    print("deu certo")
+finally:
+    print("acabou")
---
+++ 1_aulas/aula_2/2_if.py
@@ -0,0 +1,17 @@
+# if
+
+idade = 22
+
+if idade >= 18:
+    print("maior de idade")
+else:
+    print("menor de idade")
+
+# if isinstance
+
+numero = 22
+
+if isinstance(numero,int):
+    print("é inteiro")
+else:
+    print("não é inteiro")
