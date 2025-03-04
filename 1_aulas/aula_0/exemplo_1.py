282730ea5dea272ee0a72236a8ac9f5fff3ab1c4
Gabriel Terra <gabrterra@gmail.com>
2025-03-04 09:49:11 -0300

    aula 0

---
+++ 1_aulas/aula_0/exemplo_1.py
@@ -0,0 +1,16 @@
+
+# Entrada de dados
+    # erro de execução - tipagem de dados
+num1 = input("Digite um numero: ")
+num2 = input("Digite outro numero: ")
+print("soma: ",num1 + num2)
+
+    # corrigindo
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+print("soma: ",num1 + num2)
+
+    # para números float
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite outro numero: "))
+print("soma: ",num1 + num2)
