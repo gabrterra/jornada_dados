94c6619324d2d04897b1a5f53606e0cc0f295d75
gabrterra <gabrterra@gmail.com>
2025-03-30 19:33:29 -0300

    aula 6 e 7

---
+++ src/aula_06/0_dicas.py
@@ -0,0 +1,13 @@
+# pre-commit
+
+# revisar e ajustar 
+
+# pip install pre-commit
+
+# add um arquivo .pre-commit-config.yaml
+
+# commits com código padronizado
+
+# formatação: black isort flake8
+# segurança: bandit
+# padrão commit: commitezen
